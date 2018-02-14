ef2bed6a4470d392a728939282e0635d06aedfe7
TooKayAway <nhuff1@mail.csuchico.edu>
2018-02-13 17:17:11 -0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Noah Huff
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,21 @@ using namespace std;
 int main()
 {
   //declare variables
+  
+  string firstname;
+
+  string lastname;
 
   //get user input
+
+  cout << "Please Enter your First Name" << endl;
+
+  cin >> firstname;
+
+  cout << "Please Enter your Last Name" << endl;
+
+  cin >> lastname;
+
 
   //tell fortune
 
