16c357dd88496ea6d0a797c34b953318f06542e9
TooKayAway <nhuff1@mail.csuchico.edu>
2018-02-15 12:17:09 -0800

    .

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main()
 
   if ( firstname[0] == 65 || firstname[0] == 69 || firstname[0] == 73 || firstname[0] == 79 || firstname[0] == 85 || firstname[0] == 97 || firstname[0] == 101 || firstname[0] == 105 || firstname[0] == 111 || firstname[0] == 117 ) {
 
-    cout << "You are destined to be famous!" << endl;
+    cout << "you are destined to be famous!" << endl;
 
   } else {
 
@@ -45,7 +45,7 @@ int main()
 
   if ( lastname[lastname.length() - 1 ] == 65 || lastname[lastname.length() - 1 ] == 69 || lastname[lastname.length() - 1 ] == 73 || lastname[lastname.length() - 1 ] == 79 || lastname[lastname.length() - 1 ] == 85 || lastname[lastname.length() - 1 ] == 97 || lastname[lastname.length() - 1 ] == 101 || lastname[lastname.length() - 1 ] == 105 || lastname[lastname.length() - 1 ] == 111 || lastname[lastname.length() - 1 ] == 117 ) {
 
-    cout << "You have already met your true love." << endl;
+    cout << "you have already met your true love." << endl;
 
   }
   
