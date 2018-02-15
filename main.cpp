e9ba7ebcffc95e45e04c8549a2b07f8a76681c15
TooKayAway <nhuff1@mail.csuchico.edu>
2018-02-15 12:03:08 -0800

    Tells a users fortune based on the characters in their name

--- main.cpp
+++ main.cpp
@@ -33,5 +33,23 @@ int main()
 
   cout << "Your Lucky Number is " << firstname.length() << endl;
 
+  if ( firstname[0] == 65 || firstname[0] == 69 || firstname[0] == 73 || firstname[0] == 79 || firstname[0] == 85 || firstname[0] == 97 || firstname[0] == 101 || firstname[0] == 105 || firstname[0] == 111 || firstname[0] == 117 ) {
+
+    cout << "You are destined to be famous!" << endl;
+
+  } else {
+
+    cout << "You should keep a low profile" << endl; 
+
+  }
+
+  if ( lastname[lastname.length() - 1 ] == 65 || lastname[lastname.length() - 1 ] == 69 || lastname[lastname.length() - 1 ] == 73 || lastname[lastname.length() - 1 ] == 79 || lastname[lastname.length() - 1 ] == 85 || lastname[lastname.length() - 1 ] == 97 || lastname[lastname.length() - 1 ] == 101 || lastname[lastname.length() - 1 ] == 105 || lastname[lastname.length() - 1 ] == 111 || lastname[lastname.length() - 1 ] == 117 ) {
+
+    cout << "You have already met your true love." << endl;
+
+  }
+  
+  cout << "Have a nice day!" << endl;
+
   return 0;
 }
